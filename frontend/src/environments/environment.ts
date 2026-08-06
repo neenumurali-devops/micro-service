12488024ad53c8e8a85d0c77658e1b2572e4cbf7
neenumurali-devops <neenumurali25@gmail.com>
2026-08-06 13:51:17 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:80/api',
-    authServiceUrl: 'http://localhost:80/api/auth',
-    paymentServiceUrl: 'http://localhost:80/api/payments'
-  };
+  production: false,
+  apiUrl: '/api',
+  authServiceUrl: '/api/auth',
+  paymentServiceUrl: '/api/payments'
+};
